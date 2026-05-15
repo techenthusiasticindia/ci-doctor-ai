a9557874ca16e394e440b1d89acfcf383ad36554
techenthusiasticindia <metirials@gmail.com>
2026-05-15 23:09:20 +0530

    Initial project setup with Bob

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <div className="min-h-screen bg-[#0B1020] text-white">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
