0a2fc380bb58945754424e9c68d12675b118ebbc
techenthusiasticindia <metirials@gmail.com>
2026-05-16 13:11:57 +0530

    Updated project structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import Navbar from "./components/Navbar"
 import StatusCards from "./components/StatusCards"
+import FailurePanel from "./components/FailurePanel"
+import DiagnosisCard from "./components/DiagnosisCard"
+import FixPanel from "./components/FixPanel"
 
 function App() {
   return (
@@ -7,7 +10,21 @@ function App() {
       <Navbar />
 
       <div className="max-w-7xl mx-auto px-6 py-6">
+
         <StatusCards />
+
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mt-6">
+
+          <div className="space-y-6">
+            <FailurePanel />
+            <DiagnosisCard />
+          </div>
+
+          <div className="space-y-6">
+            <FixPanel />
+          </div>
+
+        </div>
       </div>
     </div>
   )
